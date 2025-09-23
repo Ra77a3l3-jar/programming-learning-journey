6f0941369d14fca796ed12012a559319f1941412
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-09-23 16:22:41 +0200

    feat: created config.py
    - Config.py is used to create a dictionary where the key will be used to
    generate the dir and the values will be used to check the extension of
    the file

---
+++ python/beginner/03-file-organizer/config.py
@@ -0,0 +1,9 @@
+FILE_TYPE_MAPPINGS = {
+    'Documents': ['.pdf', '.doc', '.docx', '.md', '.txt', '.rtf', '.odt', '.xls', '.xlsx', '.ppt', '.pptx'],
+    'Images': ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.svg', '.webp'],
+    'Audio': ['.mp3', '.wav', '.flac', '.aac', '.ogg', '.wma', '.m4a'],
+    'Video': ['.mp4', '.avi', '.mkv', '.mov', '.wmv', '.flv', '.webm', '.m4v'],
+    'Archives': ['.zip', '.rar', '.7z', '.tar', '.gz', '.bz2', '.xz'],
+    'Code': ['.py', '.s', '.rs', '.zig', '.cpp', '.c', '.java', '.go'],
+    'Executables': ['.exe', '.msi', '.deb', '.rpm', '.dmg', '.app']
+}
