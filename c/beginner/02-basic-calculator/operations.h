5dc9b6cb170a990265e7c915bb372ee82b79be74
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-09-26 21:25:22 +0200

    feat: working on a simple calculator
    - create the operation.h with all the operation functions defined

---
+++ c/beginner/02-basic-calculator/operations.h
@@ -0,0 +1,13 @@
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+
+float additions();
+float subtraction();
+float multiplication();
+float division();
+float power();
+float square_root();
+float factorial();
+float fibonacci();
+
+#endif
