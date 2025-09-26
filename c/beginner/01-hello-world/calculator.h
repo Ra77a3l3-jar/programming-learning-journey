146008190cb3d4afe623908d12ed53683ff9898d
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-09-26 21:00:26 +0200

    feat: created header files
    - Was getting and error with the Makefile since, importing .c files to
    another .c file aparently "copies" the code and i was getting the error
    that some fucntions had duplicated

---
+++ c/beginner/01-hello-world/calculator.h
@@ -0,0 +1,10 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+void addition(float a, float b);
+void subtraction(float a, float b);
+void multiplication(float a, float b);
+void division(float a, float b);
+
+#endif
+
---
+++ c/beginner/01-hello-world/user_input.h
@@ -0,0 +1,9 @@
+#ifndef USER_INPUT_H
+#define USER_INPUT_H
+
+char* getName();
+int getAge();
+void getNumbers(float *a, float *b);
+
+#endif
+
